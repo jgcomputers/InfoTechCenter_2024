2c6f0c636b5b15dbcc9070ebb2521988c7883183
Gibbs <jgcomputers132@gmail.com>
2024-02-19 13:56:15 -0500

    Add Function listOfGasStations - Called - Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -4,9 +4,17 @@ print("Gasoline Branch\n\n")
 #Import Libararies Here
 import random
 
+#Function that list Gas Levels, randomly choosing one and returning its value
 def gasLevelGauge():
     gasLevelList = ["Empty", "Low", "Quarter Tank", "Three Quarter Tank", "Full Tank"]
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
+#Function that lists Gas Stations, randomly choosing one and returning its value
+def listOfGasStations():
+    gasStations = ["Shell", "Speedway", "Marathon", "Moble", "Cosco", "Meijer", "7Eleven"]
+    gasStationsNearby = random.choice(gasStations)
+    return gasStationsNearby
+
 print(gasLevelGauge())
+print(listOfGasStations())

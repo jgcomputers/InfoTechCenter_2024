8b46c928e34d1514b8e1f988aef1ca6b7ce8bb8d
Gibbs <jgcomputers132@gmail.com>
2024-02-26 14:11:45 -0500

    Added ELIFS to VRS Function - Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -8,8 +8,42 @@ from time import sleep
 
 #Create a function that randomly chooses the weather from a list
 def weather():
-    weatherForecast = ["Snowing", "Blizzards", "Raining", "Foggy", "Windy", "Icy", "Sunny"]
+    weatherForecast = ["snowy", "blizzard", "rainy", "foggy", "windy", "icy", "sunny"]
     weatherCondition = random.choice(weatherForecast)
     return weatherCondition
 
-print(weather())
+#Variable to call the weather() once VRS(Vehicle Response System) is determined.
+weatherAlert = weather()
+
+def vehicleResponseSystem():
+    if weatherAlert == "snowy":
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been emgaged only allowing you to drive 50mps")
+    elif weatherAlert == "blizzard":
+        print("\nNational Weather Service has updated our alarm by 45 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been emgaged only allowing you to drive 40mps")
+    elif weatherAlert == "rainy":
+        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been emgaged only allowing you to drive 60mps")
+    elif weatherAlert == "foggy":
+        print("\nNational Weather Service has updated our alarm by 45 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been emgaged only allowing you to drive 40mps")
+    elif weatherAlert == "windy":
+        print("\nNational Weather Service has updated our alarm by 45 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been emgaged only allowing you to drive 40mps")
+    elif weatherAlert == "icy":
+        print("\nNational Weather Service has updated our alarm by 45 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been emgaged only allowing you to drive 40mps")
+    else:
+        print("\nNational Weather Service has updated our alarm by 45 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been emgaged only allowing you to drive 40mps")
+
+
+vehicleResponseSystem()

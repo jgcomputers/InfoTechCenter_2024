b5241dbe32d976f9b4b0295baddc38374071d13d
Gibbs <jgcomputers132@gmail.com>
2024-02-27 13:44:57 -0500

    Called Sleep Library to add time between print statements - Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -19,29 +19,36 @@ def vehicleResponseSystem():
     if weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forecast of", weatherAlert,
               "weather conditions.")
+        sleep(1)
         print("VRS has been engaged only allowing you to drive 50mps")
     elif weatherAlert == "blizzard":
         print("\nNational Weather Service has updated our alarm by 45 minutes because of the forecast of", weatherAlert,
               "weather conditions.")
+        sleep(1)
         print("VRS has been engaged only allowing you to drive 40mps")
     elif weatherAlert == "rainy":
         print("\nNational Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
               "weather conditions.")
+        sleep(1)
         print("VRS has been engaged only allowing you to drive 60mps")
     elif weatherAlert == "foggy":
         print("\nNational Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
               "weather conditions.")
+        sleep(1)
         print("VRS has been engaged only allowing you to drive 60mps")
     elif weatherAlert == "windy":
         print("\nNational Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
               "weather conditions.")
+        sleep(1)
         print("VRS has been engaged only allowing you to drive 70mps")
     elif weatherAlert == "icy":
         print("\nNational Weather Service has updated our alarm by 60 minutes because of the forecast of", weatherAlert,
               "weather conditions.")
+        sleep(1)
         print("VRS has been engaged only allowing you to drive 30mps")
     else:
         print("\nNational Weather Service forecasts", weatherAlert, "weather conditions.")
+        sleep(1)
         print("VRS has been disengaged! Drive at your own risk.")
 
 
